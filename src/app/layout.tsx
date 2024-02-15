e512e2bb562f1692e11a76c9b86097aaf9fef8c5
manapixels <chrenelias@outlook.com>
2024-02-15 21:35:53 +0800

    Add 4 new tables - users, events, permissions, roles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,59 +42,13 @@ export default function RootLayout({
               </button>
             </div>
             <div className="hidden lg:flex lg:gap-x-12">
-              <a href="#" className="text-sm font-semibold leading-6 text-gray-900">Events</a>
-              <a href="#" className="text-sm font-semibold leading-6 text-gray-900">About</a>
+              <Link href="/events" className="text-sm font-semibold leading-6 text-gray-900">Events</Link>
+              <Link href="/about" className="text-sm font-semibold leading-6 text-gray-900">About</Link>
             </div>
             <div className="hidden lg:flex lg:flex-1 lg:justify-end">
               <Link href="/auth/login" type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Log in <span aria-hidden="true">&rarr;</span></Link>
             </div>
           </nav>
-          {/* Mobile menu, show/hide based on menu open state. */}
-          <div className="lg:hidden" role="dialog" aria-modal="true">
-            {/* Background backdrop, show/hide based on slide-over state. */}
-            <div className="fixed inset-0 z-10"></div>
-            <div className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-              <div className="flex items-center justify-between">
-                <a href="#" className="-m-1.5 p-1.5">
-                  <span className="sr-only">Your Company</span>
-                  <Image
-                    className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-                    src="/logo.svg"
-                    alt="innercircle Logo"
-                    width={120}
-                    height={82}
-                    priority
-                  />
-                </a>
-                <button type="button" className="-m-2.5 rounded-md p-2.5 text-gray-700">
-                  <span className="sr-only">Close menu</span>
-                  <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
-                  </svg>
-                </button>
-              </div>
-              <div className="mt-6 flow-root">
-                <div className="-my-6 divide-y divide-gray-500/10">
-                  <div className="space-y-2 py-6">
-                    <div className="-mx-3">
-                      <button type="button" className="flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50" aria-controls="disclosure-1" aria-expanded="false">
-                        Product
-                        <svg className="h-5 w-5 flex-none" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                          <path fillRule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clipRule="evenodd" />
-                        </svg>
-                      </button>
-                    </div>
-                    <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Features</a>
-                    <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Marketplace</a>
-                    <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Company</a>
-                  </div>
-                  <div className="py-6">
-                    <Link href="/auth/login" className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Log in</Link>
-                     </div>
-                </div>
-              </div>
-            </div>
-          </div>
         </header>
         {children}
       </body>
