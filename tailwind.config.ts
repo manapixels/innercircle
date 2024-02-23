18e4f7533e456b442f793b8a1a7f0a4a144cc312
manapixels <chrenelias@outlook.com>
2024-02-23 23:00:17 +0800

    Change replacement of colors to extend

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,28 +7,33 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    colors: {
-      'base': {
-        '100': '#FCDE5C',
-        '200': '#FAD84E',
-        '300': '#F8D240',
-        '400': '#F6CC32',
-        '500': '#F4C624',
-        '600': '#927800',
-        '700': '#7A6400',
-        '800': '#625000',
-        '900': '#6e5600',
-      },
-    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        'base': {
+          '100': '#FCDE5C',
+          '200': '#FAD84E',
+          '300': '#F8D240',
+          '400': '#F6CC32',
+          '500': '#F4C624',
+          '600': '#927800',
+          '700': '#7A6400',
+          '800': '#625000',
+          '900': '#6e5600',
+        }
+      }
+    },
+    variants: {
+      extend: {
+        backgroundImage: {
+          "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+          "gradient-conic":
+            "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        },
       },
     },
   },
   plugins: [],
-  darkMode: 'class'
+  darkMode: 'class',
 };
 export default config;
+
