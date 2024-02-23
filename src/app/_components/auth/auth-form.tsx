0fb88b0a07d6a4828fcdf5519adab7ab78fbd1f5
manapixels <chrenelias@outlook.com>
2024-02-23 23:18:52 +0800

    Fix modal toggle; added padding to modal

--- src/app/_components/auth/auth-form.tsx
+++ src/app/_components/auth/auth-form.tsx
@@ -26,169 +26,172 @@ export default function AuthForm() {
     // signUpNewUser(data.email, data.password)
   };
 
+  const toggleModal = () => setShowModal(!showModal);
+
   const btn = (
     <button
       type="button"
-      onClick={() => setShowModal(!showModal)}
-      className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+      onClick={toggleModal}
+      className="text-white bg-black hover:bg-gray-900 focus:ring-4 focus:ring-base-200 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-base-600 dark:hover:bg-base-700 focus:outline-none dark:focus:ring-base-800"
     >
       Log in <span aria-hidden="true">&rarr;</span>
     </button>
   );
-console.log(showModal)
+
   if (!showModal) return <div>{btn}</div>;
 
   return (
     <div>
       {btn}
-    <div
-      id="auth-modal"
-      tabIndex={-1}
-      className="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full"
-    >
-      <div className="relative p-4 w-full max-w-2xl max-h-full">
-        <AuthForm />
-        <div className="relative bg-white rounded-lg shadow dark:bg-gray-700 w-4/12 max-w-full">
-          {/* Modal header */}
-          <div className="flex items-center justify-between px-10 py-6 border-b rounded-t dark:border-gray-600">
-            <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
-              Welcome to <i>innercircle</i>
-            </h3>
-            <button
-              type="button"
-              className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
-              data-modal-hide="default-modal"
-            >
-              <svg
-                className="w-3 h-3"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 14 14"
+      <div
+        id="auth-modal"
+        tabIndex={-1}
+        className="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-3/12 max-w-full mx-auto md:inset-0 h-[calc(100%-1rem)] max-h-full flex"
+      >
+        <div className="bg-black bg-opacity-70 fixed top-0 right-0 left-0 w-full h-full -z-1" onClick={toggleModal} />
+        <div className="relative w-full max-w-2xl max-h-full">
+          <div className="relative bg-white rounded-lg shadow dark:bg-gray-700 max-w-full">
+            {/* Modal header */}
+            <div className="flex items-center justify-between px-12 py-6 border-b rounded-t dark:border-gray-600">
+              <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
+                Welcome to <i>innercircle</i>
+              </h3>
+              <button
+                type="button"
+                onClick={toggleModal}
+                className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
               >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
-                />
-              </svg>
-              <span className="sr-only">Close modal</span>
-            </button>
-          </div>
-
-          <form
-            className="max-w-lg mx-auto px-10 py-8"
-            onSubmit={handleSubmit(onSubmit)}
-          >
-            <div className="mb-5 w-full">
-              <input
-                type="email"
-                id="email"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                required
-                placeholder="Email"
-                {...register('email', {
-                  required: true,
-                  pattern: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
-                })}
-                aria-invalid={errors.email ? 'true' : 'false'}
-              />
-              {errors.email?.type === 'required' && (
-                <p role="alert">Please enter your email</p>
-              )}
-              {errors.email?.type === 'pattern' && (
-                <p role="alert">Please enter a valid email</p>
-              )}
+                <svg
+                  className="w-3 h-3"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 14 14"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+                  />
+                </svg>
+                <span className="sr-only">Close modal</span>
+              </button>
             </div>
-            <div className="mb-5 w-full">
-              <div className="relative">
+
+            <form
+              className="max-w-lg mx-auto p-10"
+              onSubmit={handleSubmit(onSubmit)}
+            >
+              <div className="mb-5 w-full">
                 <input
-                  type={showPassword ? 'text' : 'password'}
-                  id="password"
+                  type="email"
+                  id="email"
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   required
-                  placeholder="Password"
-                  {...register('password', { required: true })}
-                  aria-invalid={errors.password ? 'true' : 'false'}
+                  placeholder="Email"
+                  {...register('email', {
+                    required: true,
+                    pattern: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                  })}
+                  aria-invalid={errors.email ? 'true' : 'false'}
                 />
-                <div className="absolute inset-y-0 right-0 pr-3 flex items-center text-sm leading-5">
-                  <GrFormView
-                    onClick={() => setShowPassword(!showPassword)}
-                    className={`h-6 text-gray-700 ${showPassword ? 'hidden' : 'block'}`}
-                    size="30"
-                  />
-                  <GrFormViewHide
-                    onClick={() => setShowPassword(!showPassword)}
-                    className={`h-6 text-gray-700 ${showPassword ? 'block' : 'hidden'}`}
-                    size="30"
-                  />
-                  {errors.password?.type === 'required' && (
-                    <p role="alert">Please enter your password</p>
-                  )}
-                </div>
+                {errors.email?.type === 'required' && (
+                  <p role="alert">Please enter your email</p>
+                )}
+                {errors.email?.type === 'pattern' && (
+                  <p role="alert">Please enter a valid email</p>
+                )}
               </div>
-            </div>
-            {state === 'login' && (
-              <div className="flex items-center mb-5">
-                <div className="flex items-center h-5">
+              <div className="mb-5 w-full">
+                <div className="relative">
                   <input
-                    id="remember"
-                    type="checkbox"
-                    value=""
-                    className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
+                    type={showPassword ? 'text' : 'password'}
+                    id="password"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     required
+                    placeholder="Password"
+                    {...register('password', { required: true })}
+                    aria-invalid={errors.password ? 'true' : 'false'}
                   />
+                  <div className="absolute inset-y-0 right-0 pr-3 flex items-center text-sm leading-5">
+                    <GrFormView
+                      onClick={() => setShowPassword(!showPassword)}
+                      className={`h-6 text-gray-700 ${showPassword ? 'hidden' : 'block'}`}
+                      size="30"
+                    />
+                    <GrFormViewHide
+                      onClick={() => setShowPassword(!showPassword)}
+                      className={`h-6 text-gray-700 ${showPassword ? 'block' : 'hidden'}`}
+                      size="30"
+                    />
+                    {errors.password?.type === 'required' && (
+                      <p role="alert">Please enter your password</p>
+                    )}
+                  </div>
                 </div>
-                <label
-                  htmlFor="remember"
-                  className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                >
-                  Keep me logged in
-                </label>
-                <a
-                  className="text-xs text-gray-400 font-medium text-foreground ml-4"
-                  href="/forgot-password"
-                >
-                  Forgot password?
-                </a>
               </div>
-            )}
-            <button
-              type="submit"
-              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm block w-full px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            >
-              {state === 'login' ? 'Log in' : 'Sign up'}
-            </button>
+              {state === 'login' && (
+                <div className="flex justify-between mb-5">
+                  <div className="flex items-center">
+                    <div className="flex items-center h-5">
+                      <input
+                        id="remember"
+                        type="checkbox"
+                        value=""
+                        className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
+                        required
+                      />
+                    </div>
+                    <label
+                      htmlFor="remember"
+                      className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                    >
+                      Keep me logged in
+                    </label>
+                  </div>
+                  <a
+                    className="text-sm text-gray-400 font-medium text-foreground ml-4"
+                    href="/forgot-password"
+                  >
+                    Forgot password?
+                  </a>
+                </div>
+              )}
+              <button
+                type="submit"
+                className="text-black bg-base-100 hover:bg-base-200 focus:ring-4 focus:outline-none focus:ring-base-300 font-bold rounded-lg text-sm block w-full px-5 py-2.5 text-center dark:bg-base-600 dark:hover:bg-base-700 dark:focus:ring-base-800"
+              >
+                {state === 'login' ? 'Log in' : 'Sign up'}
+              </button>
 
-            {state === 'login' && (
-              <div className="text-xs text-gray-500 text-center mt-4">
-                No account yet?{' '}
-                <button
-                  onClick={() => setState('register')}
-                  className="text-gray-900"
-                >
-                  Sign up
-                </button>
-              </div>
-            )}
-            {state === 'register' && (
-              <div className="text-xs text-gray-500 text-center mt-4">
-                Already have an account?{' '}
-                <button
-                  onClick={() => setState('login')}
-                  className="text-gray-900"
-                >
-                  Log in
-                </button>
-              </div>
-            )}
-          </form>
+              {state === 'login' && (
+                <div className="text-sm text-gray-500 text-center mt-4">
+                  No account yet?{' '}
+                  <button
+                    onClick={() => setState('register')}
+                    className="text-gray-900"
+                  >
+                    Sign up
+                  </button>
+                </div>
+              )}
+              {state === 'register' && (
+                <div className="text-xs text-gray-500 text-center mt-4">
+                  Already have an account?{' '}
+                  <button
+                    onClick={() => setState('login')}
+                    className="text-gray-900"
+                  >
+                    Log in
+                  </button>
+                </div>
+              )}
+            </form>
+          </div>
         </div>
       </div>
-    
-    </div>
     </div>
   );
 }
