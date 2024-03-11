79fe1730fc14e69cc9ef2efa5432fa91c602e406
manapixels <chrenelias@outlook.com>
2024-03-11 13:54:08 +0800

    Disable the middleware when the request is a prefetch call

--- src/middleware.ts
+++ src/middleware.ts
@@ -15,6 +15,13 @@ export const config = {
      * - favicon.ico (favicon file)
      * Feel free to modify this pattern to include more paths.
      */
-    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+    {
+      source: '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+      missing: [
+        { type: 'header', key: 'next-router-prefetch' },
+        { type: 'header', key: 'purpose', value: 'prefetch' },
+      ],
+    },
   ],
 }
+
