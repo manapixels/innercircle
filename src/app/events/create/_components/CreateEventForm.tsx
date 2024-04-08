f6f04d53c719893036f15339ab3562fdaa462520
manapixels <chrenelias@outlook.com>
2024-04-08 21:00:57 +0800

    Combine date fields display

--- src/app/events/create/_components/CreateEventForm.tsx
+++ src/app/events/create/_components/CreateEventForm.tsx
@@ -3,7 +3,6 @@
 import { useEffect } from 'react';
 import GooglePlacesAutocomplete from 'react-google-autocomplete';
 import { useForm, SubmitHandler } from 'react-hook-form';
-import { format, parseISO } from 'date-fns';
 import {
   getTimeZonesWithOffset,
   getGuessedUserTimeZone,
@@ -20,7 +19,6 @@ type Inputs = {
   date_end: string;
   time_start: string;
   time_end: string;
-  all_day: boolean;
   time_zone: string;
   description: string;
   price: number;
@@ -59,18 +57,8 @@ export default function CreateEventForm() {
   const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
 
   // Watch fields
-  const watchAllDay = watch('all_day');
   const watchStartDate = watch('date_start');
   const watchStartTime = watch('time_start');
-  const watchEndDate = watch('date_end');
-
-  useEffect(() => {
-    if (watchAllDay) {
-      setValue('time_start', '00:00');
-      setValue('time_end', '00:00');
-      // Optionally, adjust date_end to be the same as date_start if needed
-    }
-  }, [watchAllDay, setValue]);
 
   useEffect(() => {
     setValue('date_end', watchStartDate);
@@ -137,72 +125,72 @@ export default function CreateEventForm() {
           >
             Location <span className="text-red-500">*</span>
           </label>
+         
 
-<div className="flex flex-row gap-4">
-          <div className="relative flex-grow">
-            {/* Fields available: https://developers.google.com/maps/documentation/javascript/place-data-fields */}
-            <GooglePlacesAutocomplete
-              className={`mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 pl-9 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm bg-gray-50`}
-              apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}
-              onPlaceSelected={handlePlaceSelected}
-              options={{
-                types: ['establishment'],
-                fields: [
-                  'name',
-                  'formatted_address',
-                  'address_components',
-                  'url',
-                  'website',
-                ],
-                componentRestrictions: { country: 'SG' },
-              }}
-            />
-            <div className="absolute top-0 left-0 p-2 mt-1">
-              <svg
-                width="20px"
-                height="20px"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-                color="#434a59"
-              >
-                <path
-                  d="M17 17L21 21"
-                  stroke="#434a59"
+          <div className="flex flex-row gap-4">
+            <div className="relative flex-grow">
+              {/* Fields available: https://developers.google.com/maps/documentation/javascript/place-data-fields */}
+              <GooglePlacesAutocomplete
+                className={`mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 pl-9 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm bg-gray-50`}
+                apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}
+                onPlaceSelected={handlePlaceSelected}
+                options={{
+                  types: ['establishment'],
+                  fields: [
+                    'name',
+                    'formatted_address',
+                    'address_components',
+                    'url',
+                    'website',
+                  ],
+                  componentRestrictions: { country: 'SG' },
+                }}
+              />
+              <div className="absolute top-0 left-0 p-2 mt-1">
+                <svg
+                  width="20px"
+                  height="20px"
+                  viewBox="0 0 24 24"
                   strokeWidth="1.5"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                ></path>
-                <path
-                  d="M3 11C3 15.4183 6.58172 19 11 19C13.213 19 15.2161 18.1015 16.6644 16.6493C18.1077 15.2022 19 13.2053 19 11C19 6.58172 15.4183 3 11 3C6.58172 3 3 6.58172 3 11Z"
-                  stroke="#434a59"
-                  strokeWidth="1.5"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                ></path>
-              </svg>
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                  color="#434a59"
+                >
+                  <path
+                    d="M17 17L21 21"
+                    stroke="#434a59"
+                    strokeWidth="1.5"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  ></path>
+                  <path
+                    d="M3 11C3 15.4183 6.58172 19 11 19C13.213 19 15.2161 18.1015 16.6644 16.6493C18.1077 15.2022 19 13.2053 19 11C19 6.58172 15.4183 3 11 3C6.58172 3 3 6.58172 3 11Z"
+                    stroke="#434a59"
+                    strokeWidth="1.5"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  ></path>
+                </svg>
+              </div>
             </div>
-            
-          </div>
-          <div>
-            <select
+            <div>
+              <select
                 {...register('location_country', {
-                required: 'Please select the country of the location.',
+                  required: 'Please select the country of the location.',
                 })}
                 name="location_country"
                 id="location_country"
                 defaultValue="Singapore"
                 disabled={true}
                 className={`mt-1 block w-full border ${errors.location_country ? 'border-red-500' : 'border-gray-300'} rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm bg-gray-50 pointer-events-none`}
-            >
+              >
                 <option>Singapore</option>
-            </select>
-            {errors.location_country && (
+              </select>
+              {errors.location_country && (
                 <span className="text-red-500">
-                {errors.location_country.message}
+                  {errors.location_country.message}
                 </span>
-            )}
+              )}
             </div>
           </div>
         </div>
@@ -223,7 +211,7 @@ export default function CreateEventForm() {
           )}
         </div>
 
-            <div className="-mt-3">
+        <div className="-mt-3">
           <input
             {...register('location_address', {
               required: 'Please enter the address of the location.',
@@ -240,15 +228,35 @@ export default function CreateEventForm() {
               {errors.location_address.message}
             </span>
           )}
-          </div>
-        
-        <div>
+        </div>
+
+        <div className="sm:col-span-2">
+            <div className="flex justify-between">
           <label
-            htmlFor="date_start"
             className="block text-sm font-medium text-gray-700"
           >
-            Start Date <span className="text-red-500">*</span>
+            Date <span className="text-red-500">*</span>
           </label>
+          <select
+            {...register('time_zone', {
+              required: 'Please select the time zone of the event.',
+            })}
+            name="time_zone"
+            id="time_zone"
+            defaultValue={guessedTimeZone}
+            required
+            className={`block border ${errors.time_zone ? 'border-red-500' : 'border-gray-300'} rounded-md shadow-sm py-1 px-1 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-xs bg-gray-50`}
+          >
+            {timeZones.map((zone) => (
+              <option key={zone} value={zone}>
+                {zone}
+              </option>
+            ))}
+          </select>
+          </div>
+
+          <div className="grid grid-cols-[1fr_1fr_20px_1fr_1fr] gap-4">
+            <div>
           <input
             {...register('date_start', {
               required: 'Please select the date of the event.',
@@ -262,48 +270,8 @@ export default function CreateEventForm() {
           {errors.date_start && (
             <span className="text-red-500">{errors.date_start.message}</span>
           )}
-          {/* Display formatted date_start for user reference */}
-        <div className="text-sm text-gray-600">
-            {watchStartDate && format(parseISO(watchStartDate), 'EEE, MMMM d')}
-        </div>
-        </div>
-        {!watchAllDay && (
-          <div>
-            <label
-              htmlFor="date_end"
-              className="block text-sm font-medium text-gray-700"
-            >
-              End Date <span className="text-red-500">*</span>
-            </label>
-            <input
-              {...register('date_end', {
-                required: 'Please select the end date of the event.',
-                validate: value =>
-                  new Date(value) >= new Date(watchStartDate) || 'End date must be after start date.',
-              })}
-              type="date"
-              name="date_end"
-              id="date_end"
-              required
-              className={`mt-1 block w-full border ${errors.date_end ? 'border-red-500' : 'border-gray-300'} rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm bg-gray-50`}
-            />
-            {errors.date_end && (
-              <span className="text-red-500">{errors.date_end.message}</span>
-            )}
-            {/* Display formatted date_end for user reference */}
-  <div className="text-sm text-gray-600">
-    {watchEndDate && format(parseISO(watchEndDate), 'EEE, MMMM d')}
-  </div>
           </div>
-        )}
-        <div className="grid grid-cols-2 gap-4">
           <div>
-            <label
-              htmlFor="time_start"
-              className="block text-sm font-medium text-gray-700"
-            >
-              Start Time <span className="text-red-500">*</span>
-            </label>
             <input
               {...register('time_start', {
                 required: 'Please enter the start time of the event.',
@@ -318,13 +286,8 @@ export default function CreateEventForm() {
               <span className="text-red-500">{errors.time_start.message}</span>
             )}
           </div>
+          <div className="text-center self-center">–</div>
           <div>
-            <label
-              htmlFor="time_end"
-              className="block text-sm font-medium text-gray-700"
-            >
-              End Time <span className="text-red-500">*</span>
-            </label>
             <input
               {...register('time_end', {
                 required: 'Please enter the end time of the event.',
@@ -339,49 +302,27 @@ export default function CreateEventForm() {
               <span className="text-red-500">{errors.time_end.message}</span>
             )}
           </div>
+          <div>
+            <input
+              {...register('date_end', {
+                required: 'Please select the end date of the event.',
+                validate: (value) =>
+                  new Date(value) >= new Date(watchStartDate) ||
+                  'End date must be after start date.',
+              })}
+              type="date"
+              name="date_end"
+              id="date_end"
+              required
+              className={`mt-1 block w-full border ${errors.date_end ? 'border-red-500' : 'border-gray-300'} rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm bg-gray-50`}
+            />
+            {errors.date_end && (
+              <span className="text-red-500">{errors.date_end.message}</span>
+            )}
+          </div>
+          </div>
         </div>
-        <div className="flex items-center">
-          <input
-            {...register('all_day')}
-            type="checkbox"
-            name="all_day"
-            id="all_day"
-            className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded bg-gray-50"
-          />
-          <label
-            htmlFor="all_day"
-            className="ml-2 block text-sm font-medium text-gray-700"
-          >
-            All day
-          </label>
-        </div>
-        <div>
-          <label
-            htmlFor="time_zone"
-            className="block text-sm font-medium text-gray-700"
-          >
-            Time zone <span className="text-red-500">*</span>
-          </label>
-          <select
-            {...register('time_zone', {
-              required: 'Please select the time zone of the event.',
-            })}
-            name="time_zone"
-            id="time_zone"
-            defaultValue={guessedTimeZone}
-            required
-            className={`mt-1 block w-full border ${errors.time_zone ? 'border-red-500' : 'border-gray-300'} rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm bg-gray-50`}
-          >
-            {timeZones.map((zone) => (
-              <option key={zone} value={zone}>
-                {zone}
-              </option>
-            ))}
-          </select>
-          {errors.time_zone && (
-            <span className="text-red-500">{errors.time_zone.message}</span>
-          )}
-        </div>
+        
         <div>
           <label
             htmlFor="description"
@@ -472,4 +413,3 @@ export default function CreateEventForm() {
     </form>
   );
 }
-
