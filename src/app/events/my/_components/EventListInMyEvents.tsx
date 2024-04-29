3734d757abb2b9861893506b168787fe2146cdb6
manapixels <chrenelias@outlook.com>
2024-04-29 10:55:27 +0800

    Add empty state to EventList

--- src/app/events/my/_components/EventListInMyEvents.tsx
+++ src/app/events/my/_components/EventListInMyEvents.tsx
@@ -5,11 +5,13 @@ import { useUser } from '@/_contexts/UserContext';
 import EventListItemInMyEvents from './EventListItemInMyEvents';
 import { ProfileWithRoles } from '@/types/profile';
 import { fetchUserProfile } from '@/api/profile';
+import { hasDatePassed } from '@/helpers/date';
 import { Event } from '@/types/event';
 
 export default function EventListInMyEvents() {
   const { user } = useUser();
-  const [signedUpEvents, setSignedUpEvents] = useState<Event[] | undefined>();
+  const [pastEvents, setPastEvents] = useState<Event[]>([]);
+  const [futureEvents, setFutureEvents] = useState<Event[]>([]);
 
   useEffect(() => {
     const fetchEvents = async () => {
@@ -18,7 +20,9 @@ export default function EventListInMyEvents() {
 
         console.log(result);
         if (result?.signed_up_events) {
-          setSignedUpEvents(result.signed_up_events as Event[]);
+          const events = result.signed_up_events as Event[];
+          setPastEvents(events.filter(event => hasDatePassed(event.date_end)));
+          setFutureEvents(events.filter(event => !hasDatePassed(event.date_end)));
         }
       }
     };
@@ -26,12 +30,39 @@ export default function EventListInMyEvents() {
   }, [user]);
 
   return (
-    <div
-      className={`grid grid-cols-2 lg:grid-cols-4 gap-6 bg-gray-50 rounded-2xl p-8`}
-    >
-      {signedUpEvents?.map((event, i) => {
-        return <EventListItemInMyEvents event={event} key={i} />;
-      })}
+    <div className="space-y-8">
+      <div>
+        <h2 className="font-medium text-xl mb-4">Upcoming events</h2>
+        <div
+          className="grid grid-cols-2 lg:grid-cols-4 gap-6 bg-gray-50 rounded-2xl p-8"
+        >
+          {futureEvents.length > 0 ? (
+            futureEvents.map((event, i) => (
+              <EventListItemInMyEvents event={event} key={i} />
+            ))
+          ) : (
+            <div className="text-center text-gray-500">
+              No upcoming events.
+            </div>
+          )}
+        </div>
+      </div>
+      <div>
+        <h2 className="font-medium text-xl mb-4">Past events</h2>
+        <div
+          className="grid grid-cols-2 lg:grid-cols-4 gap-6 bg-gray-50 rounded-2xl p-8"
+        >
+          {pastEvents.length > 0 ? (
+            pastEvents.map((event, i) => (
+              <EventListItemInMyEvents event={event} key={i} />
+            ))
+          ) : (
+            <div className="text-center text-gray-500">
+              No past events.
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
--- src/app/events/my/page.tsx
+++ src/app/events/my/page.tsx
@@ -10,7 +10,6 @@ export default function MyEventsPage() {
   return (
     <ProtectedWrapper>
       <div className="flex w-full flex-col md:col-span-4">
-        <h2 className="font-bold text-xl mb-4">My Events</h2>
         <EventListInMyEvents />
       </div>
     </ProtectedWrapper>
