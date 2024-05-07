f110347d50a45570bbf836823d57360871e1e530
manapixels <chrenelias@outlook.com>
2024-05-07 20:09:38 +0800

    Update next config

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,7 @@ const nextConfig = {
         remotePatterns: [
             { protocol: 'http', hostname: '127.0.0.1' },
             { protocol: 'http', hostname: 'localhost' },
+            { protocol: 'https', hostname: 'xrraiyxtanthtstytoxt.supabase.co' },
         ],
     },
 };
